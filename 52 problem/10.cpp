c33b9c0d081485c5462506d39c3bf7c149f2dd9c
Atiqur Rahman <mdatikullah88@gmail.com>
2019-03-16 23:55:23 +0600

    Dimik_Oj

--- 52 problem/10.cpp
+++ 52 problem/10.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main () {
     int t,r1,r2,b;
-    double c_r,a_r,ball_played;
+    double c_r,r_r,ball_played;
 
     cin >> t;
     while (t--) {
         cin >> r1 >> r2 >> b;
         ball_played=300-b;
-        cout << ball_played << endl;
+        ball_played*=1.0;
         c_r=(r2/ball_played)*6;
-
-        a_r=((r1-r2+1)/b)*6;
-
-        cout << c_r << " " << a_r << endl;
-
-        printf("%0.2lf %0   .2lf\n",c_r,a_r);
+        r_r=(((r1-r2+1)*1.0)/b)*6;
+        printf("%0.2lf %0.2lf\n",c_r,r_r);
     }
+
     return 0;
 }
